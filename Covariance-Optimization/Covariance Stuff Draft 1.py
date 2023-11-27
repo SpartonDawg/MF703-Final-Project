ca9c9895818b2a389ae238bfbc6d0ab8e022c432
kevyyang <150382377+kevyyang@users.noreply.github.com>
2023-11-26 21:45:42 -0500

    Added some comments, cleaned up some stuff

--- Covariance-Optimization/Covariance Stuff Draft 1.py
+++ Covariance-Optimization/Covariance Stuff Draft 1.py
@@ -17,31 +17,16 @@ from sklearn.linear_model import LinearRegression
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-# Artificial Testing Data 
-test_ret = pd.DataFrame(np.random.randn(25, 19), 
-                        index = pd.date_range('2023-01-01', periods = 25), 
-                        columns = [f"Commodity {i}" for i in range(1, 20)])
-
-test_strat = pd.DataFrame(np.random.randint(-1, 2, size= (25, 19)), 
-                        index = pd.date_range('2023-01-01', periods = 25), 
-                        columns = [f"Commodity {i}" for i in range(1, 20)])
-
-test_syn_index = pd.DataFrame(np.random.randn(25,1), 
-                        index = pd.date_range('2023-01-01', periods = 25), 
-                        columns = ["Commodity Index"])
-
-test_date = '2023-1-22'
-
 class WeightOptimization:
     
     def __init__(self, df_syn_index, df_ret, df_strategy):
-        self.df_syn_index = df_syn_index
-        self.df_ret = df_ret
-        self.df_strategy = df_strategy
+        self.df_syn_index = df_syn_index # pandas Series indexed by date containing the index returns on that date
+        self.df_ret = df_ret # dataframe indexed by date and with columns labeled by commodities
+        self.df_strategy = df_strategy # dataframe indexed by date and with columns labeled by commodities
         self.num_securities = len(df_ret.columns)
         
     def slicer(self, date):
-        """slices df_ret to output TTM data for commodities with non-zero positions"""
+        """slices df_ret to output TTM (trailing twelve months) data for commodities with non-zero positions"""
         sliced = pd.DataFrame(index = self.df_ret.index)
         date_index = self.df_ret.index.get_loc(date)
         
@@ -58,14 +43,15 @@ class WeightOptimization:
         return sliced
     
     def get_covar(self, date):
-        """outputs TTM covariance matrix for commodities with non-zero positions"""
+        """computes the TTM covariance matrix for commodities with non-zero positions and outputs it as a dataFrame
+        """
         sliced_data = self.slicer(date)
         cov_matrix = sliced_data.cov()
         
         return cov_matrix
         
     def get_betas(self, date):
-        """outputs TTM ßs for commodities with non-zero positions"""
+        """computes the TTM ßs for commodities with non-zero positions and outputs it as a pandas Series"""
         sliced_data = self.slicer(date)
         sliced_index = self.df_syn_index.loc[sliced_data.index] 
         betas = []
@@ -84,7 +70,10 @@ class WeightOptimization:
         return pd.Series(betas, index = sliced_data.columns)
         
     def calculate_weights(self, date, alpha=100):
-        """outputs appropriate weights given minimisation function"""
+        """
+        computes the optimal weights for a certain date and risk preference alpha
+        outputs it as a numpy array with length equal to the number of commodities in the universe
+        """
         #get today's strategy
         strat_today = self.df_strategy.loc[date].values
         #remove 0s
@@ -94,6 +83,7 @@ class WeightOptimization:
         C = self.get_covar(date)
         betas = self.get_betas(date)
         
+        #define quantity to be minimized
         def objective_function(w):
            term1 = alpha * (w @ np.transpose(betas))**2
            term2 = w @ C @ np.transpose(w)
@@ -102,11 +92,15 @@ class WeightOptimization:
         # Create a function to generate constraints based on the strategy
         def generate_constraints(strategy):
             constraints = []
+            
+            #direction constraints: weights must be positive or negative as dictated by strategy
             for i, s in enumerate(strategy):
                 if s == 1:
                     constraints.append({'type': 'ineq', 'fun': lambda weights, i=i: weights[i]})
                 elif s == -1:
                     constraints.append({'type': 'ineq', 'fun': lambda weights, i=i: -weights[i]})
+                    
+            # leverage constraint: absolute values of weights add to 1
             constraints.append({'type': 'eq', 'fun': lambda weights: np.abs(weights).sum() - 1})
             return constraints
             
@@ -119,27 +113,33 @@ class WeightOptimization:
         # Perform the optimization
         result = minimize(fun=objective_function, x0=initial_guess, constraints=constraints)
         if result.success:
-            optimized = result.x
+            optimized = result.x # store the optimal weights
         else:
             raise ValueError("Optimization failed.")
-            
+        
+        #re-insert the zero weights for all the commodities with position 0
         final_weights = np.zeros(self.num_securities)
         index = 0
         for i in range(len(strat_today)):
             if strat_today[i] != 0:
                 final_weights[i] = optimized[index]
                 index += 1
-                
+        
+        #return final result
         return final_weights
     
     def plot_frontier(self, date):
+        """
+        Given a certain date, plots the maximum neutrality frontier: the optimum correlation to market vs portfolio variance curve as alpha varies from 1 to 200
+        """
+        
         C = self.get_covar(date)
         betas = self.get_betas(date)
         
         x = np.empty(1999)
         y = np.empty(1999)
         
-        for a in range(1, 2000):
+        for a in range(1, 200):
             weights = self.calculate_weights(date, a)
             w = [v for v in weights if v != 0]
             beta_squared = (w @ np.transpose(betas))**2
@@ -154,23 +154,36 @@ class WeightOptimization:
         plt.title('minimum beta variance frontier')
         
 if __name__ == '__main__':
+    
+    # Artificial Testing Data 
+    test_ret = pd.DataFrame(np.random.randn(25, 19), 
+                            index = pd.date_range('2023-01-01', periods = 25), 
+                            columns = [f"Commodity {i}" for i in range(1, 20)])
+
+    test_strat = pd.DataFrame(np.random.randint(-1, 2, size= (25, 19)), 
+                            index = pd.date_range('2023-01-01', periods = 25), 
+                            columns = [f"Commodity {i}" for i in range(1, 20)])
+
+    test_syn_index = pd.Series(np.random.randn(25), 
+                            index = pd.date_range('2023-01-01', periods = 25), 
+                            name = "Commodity Index")
+
+    test_date = '2023-1-22'
+    
     # Testing:
     weight_optimizer = WeightOptimization(test_syn_index, test_ret, test_strat)
-    
     sliced_data = weight_optimizer.slicer(test_date)
     covar_mat = weight_optimizer.get_covar(test_date)
     b = weight_optimizer.get_betas(test_date)
     w = weight_optimizer.calculate_weights(test_date, alpha=50)
     
-    #weight_optimizer.plot_frontier(test_date)
-    
+    weight_optimizer.plot_frontier(test_date) 
     
     print("TTM Covariance Matrix:")
     print(covar_mat, "\n")
     print("TTM Commodity Betas:")
     print(b, "\n")
-    print("Strategies:")
+    print("Strategy:")
+    print(test_strat.loc[test_date].values, "\n")
+    print("Weights:")
     print(w, "\n")
-
-      
-      
