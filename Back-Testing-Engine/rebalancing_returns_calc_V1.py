73ba5f8327e4c213bfa3197f41b1e91706f2736f
jimburrill <150738500+jimburrill@users.noreply.github.com>
2023-12-01 14:44:21 -0500

    Update rebalancing_returns_calc_V1.py

--- Back-Testing-Engine/rebalancing_returns_calc_V1.py
+++ Back-Testing-Engine/rebalancing_returns_calc_V1.py
@@ -10,6 +10,12 @@ import pandas_market_calendars as mktcal
 
 
 def returns_rebalancing(scaled_daily_returns_df, position_weights_df, rebalancing_dates_df, transaction_costs = 0.0005):   
+    ''This function calculates the returns of the overall strategy when reblancing is performed according
+       to the portfolio_reblancing_identification funtion.
+       Inputs: scaled_daily_returns_df, a dataframe of sclaed daily returns based on the trading strategy,
+       position_weights_df, a dataframe containing weights to be taken in each position reccomended by the trading
+       strategy based on the output of the calculate_weights function,
+       Outputs: total_returns_df, a dataframe that contains the total strategy returns'''
     
     selected_indices = rebalancing_dates_df.index[rebalancing_dates_df > 0].tolist()
     all_port_returns = []
@@ -26,7 +32,12 @@ def returns_rebalancing(scaled_daily_returns_df, position_weights_df, rebalancin
 
 
 def daily_returns(prices_df, positions_df):
-    
+    '''Calculates the daily returns of the different commodites based on the signals provided by the
+       trading strategy.
+       Inputs: prices_df, df containing daily price data for the different commodities,
+       positions_df, df containing the recommended positions for each commodity based on the trading strategy
+       Outputs: scaled_daily_returns, df containing scaled daily returns for each commodity in our universe'''
+
     daily_returns_df =  prices_df.shift(-2).pct_change()
     scaled_daily_returns = 1+(daily_returns_df * positions_df)
     
